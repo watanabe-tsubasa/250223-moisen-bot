489bf4754666cce654648eec4dcb9320dbcdfd87
watanabe-tsubasa <t.watanabe423@gmail.com>
2025-02-23 14:18:56 +0900

    add echo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,110 @@
-import { Hono } from 'hono'
+import { 
+  messagingApi,
+  webhook,
+} from '@line/bot-sdk'
+import { ExecutionContext, Hono } from 'hono'
+import HmacSHA256 from "crypto-js/hmac-sha256";
+import Base64 from "crypto-js/enc-base64";
 
-const app = new Hono()
+type Bindings = {
+  CHANNEL_ACCESS_TOKEN: string,
+  CHANNEL_SECRET: string,
+  // WEBHOOK_URL: string,
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello hono & line!')
 })
 
+app.post('/webhook', async (c) => {
+  const body = await c.req.text() // JSONではなくテキストで取得
+  const channelAccessToken = c.env.CHANNEL_ACCESS_TOKEN || ''
+  const channelSecret = c.env.CHANNEL_SECRET || ''
+  // const webhookURL = c.env.WEBHOOK_URL || ''
+
+  // シグネチャの取得
+  const signature = c.req.header('x-line-signature')
+  if (!signature) {
+    return c.text('Missing signature', 400)
+  }
+
+  // HMACを使ってシグネチャを生成
+  const hash = await generateHmac(channelSecret, body);
+
+  // シグネチャの検証
+  if (signature !== hash) {
+    return c.text('Invalid signature', 403)
+  }
+
+  const events = JSON.parse(body).events
+  const promises = events.map((event: webhook.Event) => handleEvent(
+    event,
+    channelAccessToken,
+    // webhookURL,
+    c.executionCtx
+  ))
+  await Promise.all(promises)
+
+  return c.text('OK')
+})
+
+const handleEvent = async (
+  event: webhook.Event,
+  accessToken: string,
+  // webhookURL: string,
+  ctx: ExecutionContext
+) => {
+  if (event.type !== 'message' || event.message.type !== 'text') return;
+
+  try {
+    fetch('https://api.line.me/v2/bot/chat/loading/start', {
+      method: 'POST',
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({"chatId": event.source?.userId})
+    })
+
+    const { text } = event.message;
+    ctx.waitUntil(
+      (async () => {
+        // const res = await fetch(webhookURL, {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   },
+        //   body: JSON.stringify(event)
+        // })
+        // const text = await res.text();
+        if(!event.replyToken) return
+        const responseBody: messagingApi.ReplyMessageRequest = {
+          replyToken: event.replyToken,
+          messages: [
+            {'type': 'text', 'text': text}
+          ] 
+        }
+        
+        return fetch('https://api.line.me/v2/bot/message/reply', {
+          method: 'POST',
+          headers: {
+            "Authorization": `Bearer ${accessToken}`,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(responseBody)
+        }) 
+      })()
+    )
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export default app
+
+const generateHmac = async (secret: string, message: string) => {
+  const hmac = HmacSHA256(message, secret);
+  return Base64.stringify(hmac);
+}
